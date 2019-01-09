dc99f470f3f7efff64ddb259367fbb13b308b480
nanocheeze <cybershrapnel@nanocheeze.com>
2019-01-09 05:55:31 -0800

    Update rpcrawtransaction.cpp

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -209,7 +209,8 @@ Value listunspent(CWallet* pWallet, const Array& params, bool fHelp)
             CTxDestination address;
             if (ExtractDestination(pk, address))
             {
-                const CScriptID& hash = boost::get<const CScriptID&>(address);
+                const CScriptID& hash = boost::get<CScriptID&>(address);
+                //const CScriptID& hash = boost::get<const CScriptID&>(address);
                 CScript redeemScript;
                 if (pWallet->GetCScript(hash, redeemScript))
                     entry.push_back(Pair("redeemScript", HexStr(redeemScript.begin(), redeemScript.end())));
