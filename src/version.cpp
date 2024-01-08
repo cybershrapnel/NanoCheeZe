b0e73f462affe17ceff97126ff47c33950f19bcd
nanocheeze <cybershrapnel@nanocheeze.com>
2024-01-08 04:55:35 -0800

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("NanoCheeZe");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-V2.1"
+#define CLIENT_VERSION_SUFFIX   "-V3.1"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
